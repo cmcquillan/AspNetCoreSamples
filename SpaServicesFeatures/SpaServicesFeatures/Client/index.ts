e0a239a62608f2e473d865e1e5964895cc146c07
Casey McQuillan <Casey@quilltechnologies.com>
2019-05-15 20:55:22 -0700

    Adding Sample for spa services.

---
+++ SpaServicesFeatures/SpaServicesFeatures/Client/index.ts
@@ -0,0 +1,13 @@
+﻿
+export class MySampleClass {
+
+    constructor() {
+
+    }
+
+    showAlert(text: string) {
+        window.alert('Sample Class Alert: ' + text);
+    }
+}
+
+(<any>window).Sample = new MySampleClass();
---
+++ SpaServicesFeatures/SpaServicesFeatures/webpack.config.js
@@ -0,0 +1,28 @@
+﻿var path = require('path');
+
+module.exports = env => {
+    env = env || {};
+
+    var output = {
+        publicPath: '/dist/',
+        filename: '[name].js'
+    };
+
+    // Only create files in the target directory if it's a production environment.
+    if (env.production) {
+        output.path = path.join(__dirname, 'wwwroot', 'dist');
+    }
+
+    return {
+        entry: { 'index': './Client/index.ts' },
+        resolve: {
+            extensions: ['.js', '.ts']
+        },
+        module: {
+            rules: [
+                { test: /\.ts$/, use: 'ts-loader' }
+            ]
+        },
+        output: output
+    };
+}
